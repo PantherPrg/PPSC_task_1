03f4c5cc91a35fee8be5d9fb93b1a67a0199fee6
paulshim100 <74570815+paulshim100@users.noreply.github.com>
2020-11-25 16:04:07 -0800

    Delete answer.py

--- answer.py
+++
@@ -1,6 +0,0 @@
-# don't use unless you actually actually actually need it 
-
-def lunchPunishment(name, times): 
-  #  write your code here 
-  
-  return ("I, " + name + ", am sorry for sleeping in class.\n") * times
